9e16a47de40cb0440507c73456b06cf85e988a15
Administrator <Administrator@AE6ZA7WZKTMUEGM>
2017-09-30 17:40:13 +0800

    yarn

--- hadoop/hadoop-yarn/src/main/java/org/hadoop/yarn/App.java
+++ hadoop/hadoop-yarn/src/main/java/org/hadoop/yarn/App.java
@@ -1,8 +1,8 @@
 package org.hadoop.yarn;
 
 /**
- * Hello world!
- *
+ * http://mp.weixin.qq.com/s?__biz=MzI1MTE5NTQ0OA==&mid=402959542&idx=1&sn=dddbc34e579f0e0da14db3d97c61a1d3#rd
+ *Hadoop Yarn内存资源隔离实现原理
  */
 public class App 
 {
