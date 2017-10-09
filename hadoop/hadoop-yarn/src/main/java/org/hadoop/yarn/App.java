7b169c1d17cdf2349c2c9a7dc5df096f1d6549bd
Administrator <Administrator@AE6ZA7WZKTMUEGM>
2017-10-09 17:51:48 +0800

    yarn

--- hadoop/hadoop-yarn/src/main/java/org/hadoop/yarn/App.java
+++ hadoop/hadoop-yarn/src/main/java/org/hadoop/yarn/App.java
@@ -1,6 +1,7 @@
 package org.hadoop.yarn;
 
 /**
+ * http://dongxicheng.org/mapreduce-nextgen/understand-yarn-container-concept/
  * http://mp.weixin.qq.com/s?__biz=MzI1MTE5NTQ0OA==&mid=402959542&idx=1&sn=dddbc34e579f0e0da14db3d97c61a1d3#rd
  *Hadoop Yarn内存资源隔离实现原理
  */
